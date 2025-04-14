0ada6ae14175a4221ab412668b40c4ec32b261ac
arrjon <jonas.arruda@uni-bonn.de>
2025-04-14 14:16:19 +0200

    update experiments

--- sbatch.sh
+++ sbatch.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition gn
 #SBATCH --gpus 1
 #SBATCH --time 3-00:00:00
-#SBATCH --array=0-29%3
+#SBATCH --array=20-29
 #SBATCH --depend=afterany:2063854
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jonas.arruda@uni-bonn.de
@@ -16,7 +16,7 @@ export TQDM_DISABLE
 
 source /home/jonas/hierarchical-abi/env.sh
 
-#python3.11 /home/jonas/hierarchical-abi/gaussian_flat_score_matching.py 1
+python3.11 /home/jonas/hierarchical-abi/gaussian_flat_score_matching.py 1
 #python3.11 /home/jonas/hierarchical-abi/gaussian_flat_score_matching.py 100
-python3.11 /home/jonas/hierarchical-abi/ar\(1\)_score_matching.py 1
+#python3.11 /home/jonas/hierarchical-abi/ar\(1\)_score_matching.py 1
 #python3.11 /home/jonas/hierarchical-abi/ar\(1\)_score_matching.py 100
