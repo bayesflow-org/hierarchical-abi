3ccb85de630bda0194c57f4f2f26b8efcfe23956
arrjon <jonas.arruda@uni-bonn.de>
2025-05-02 15:02:00 +0200

    Update changes

--- sbatch_marvin.sh
+++ sbatch_marvin.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name hierarchical-fli
-#SBATCH --output log/log_hierarchical_fli.%j.txt
+#SBATCH --job-name hierarchical-linear
+#SBATCH --output log/log_hierarchical_linear.%j.txt
 #SBATCH --nodes 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem-per-cpu 100GB
 #SBATCH --partition mlgpu_long
 #SBATCH --gpus 1
 #SBATCH --time 2-00:00:00
-#SBATCH --array=0-39
-#SBATCH --depend=afterany:20604878
+#SBATCH --array=20-29
+#SBATCH --depend=afterany:20608877
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jonas.arruda@uni-bonn.de
 
@@ -18,8 +18,8 @@ export KERAS_BACKEND=torch
 
 source /home/jarruda_hpc/hierarchical-abi/env_marvin.sh
 
-#python3.11 /home/jarruda_hpc/hierarchical-abi/gaussian_flat_score_matching.py 1
+python3.11 /home/jarruda_hpc/hierarchical-abi/gaussian_flat_score_matching.py 1
 #python3.11 /home/jarruda_hpc/hierarchical-abi/gaussian_flat_score_matching.py 100
 #python3.11 /home/jarruda_hpc/hierarchical-abi/ar\(1\)_score_matching.py 1
 #python3.11 /home/jarruda_hpc/hierarchical-abi/ar\(1\)_score_matching.py 100
-python3.11 /home/jarruda_hpc/hierarchical-abi/fli\ score\ matching.py
+#python3.11 /home/jarruda_hpc/hierarchical-abi/fli\ score\ matching.py
